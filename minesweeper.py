3b02f772b5c2bd1afd555733a5847bbd0a1f8bf8
Boss68 <43830774+Boss68@users.noreply.github.com>
2020-01-21 22:07:24 -0500

    Add files via upload

--- minesweeper.py
+++ minesweeper.py
@@ -1,58 +1,87 @@
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QCursor
-import sys, threading, pygame
+from PyQt5.Qt import *
+import sys, PIL
 
-difficulty=0
-pygame.init()
-def pygameStuff():
-    global window
-    minesweeper=pygame.Surface()
-    while True:
-        mousePos=(QCursor.pos().x()-window.frameGeometry().topLeft().x(),QCursor.pos().y()-window.frameGeometry().topLeft().y()-23)
-pygameThread=threading.Thread(target=pygameStuff)
-def buttonPressed(action):
-    global difficulty
-    if action.text()=="Beginner":
-        difficulty=0
-    elif action.text()=="Intermediate":
-        difficulty=1
-    elif action.text()=="Expert":
-        difficulty=2
-    elif action.text()=="Custom...":
-        difficulty=3
-
-    print(action.text() +" pressed.")
+class MinesweeperWindow(QMainWindow):
+    def __init__(self):
+        super(QMainWindow,self).__init__()
+        self.m = QMenuBar(self)
+        self.game = self.m.addMenu("Game")
+        self.newGame = QAction("New",self)
+        self.newGame.setShortcut("F2")
+        self.game.addAction(self.newGame)
+        self.game.addSeparator()
+        self.beginner = QAction("Beginner",self)
+        self.beginner.setCheckable(True)
+        self.beginner.setChecked(True)
+        self.game.addAction(self.beginner)
+        self.intermediate = QAction("Intermediate",self)
+        self.intermediate.setCheckable(True)
+        self.game.addAction(self.intermediate)
+        self.expert = QAction("Expert",self)
+        self.expert.setCheckable(True)
+        self.game.addAction(self.expert)
+        self.custom = QAction("Custom...",self)
+        self.custom.setCheckable(True)
+        self.game.addAction(self.custom)
+        self.game.addSeparator()
+        marks = QAction("Marks(?)",self)
+        marks.setCheckable(True)
+        marks.setChecked(True)
+        self.game.addAction(marks)
+        color = QAction("Color",self)
+        color.setCheckable(True)
+        color.setChecked(True)
+        self.game.addAction(color)
+        sound = QAction("Sound",self)
+        sound.setCheckable(True)
+        sound.setChecked(False)
+        self.game.addAction(sound)
+        self.game.addSeparator()
+        self.game.addAction("Best Times...")
+        self.game.addSeparator()
+        exit_ = QAction("Exit_",self)
+        self.game.addAction(exit_)
+        help = self.m.addMenu("Help")
+        contents=QAction("Contents",self)
+        contents.setShortcut("F1")
+        help.addAction(contents)
+        self.game.triggered[QAction].connect(self.buttonPressed)
+        exit_.triggered.connect(sys.exit)
+        self.setGeometry(1280, 720, 200, 50)
+        timer = QTimer(self)
+        timer.timeout.connect(self.pygameLoop)
+        timer.start(1)
+        self.setWindowTitle("Minesweeper")
+        self.show()
+    def pygameLoop(self):
+        global window
+        mousePos = (QCursor.pos().x() - window.frameGeometry().topLeft().x(),
+                    QCursor.pos().y() - window.frameGeometry().topLeft().y() - 23)
+        # print(mousePos)
+    def buttonPressed(self,action):
+        if action.text() == "Beginner":
+            self.intermediate.setChecked(False)
+            self.expert.setChecked(False)
+            self.custom.setChecked(False)
+        elif action.text() == "Intermediate":
+            self.beginner.setChecked(False)
+            self.expert.setChecked(False)
+            self.custom.setChecked(False)
+        elif action.text() == "Expert":
+            self.beginner.setChecked(False)
+            self.intermediate.setChecked(False)
+            self.custom.setChecked(False)
+        elif action.text() == "Custom...":
+            self.beginner.setChecked(False)
+            self.intermediate.setChecked(False)
+            self.expert.setChecked(False)
+        print(action.text() + " pressed.")
+    def mouseReleaseEvent(self, QMouseEvent):
+        if QMouseEvent.button()==Qt.LeftButton:
+            print("left button")
+        elif QMouseEvent.button()==Qt.RightButton:
+            print("right button")
 app=QApplication(sys.argv)
-window=QMainWindow()
-m=QMenuBar(window)
-game=m.addMenu("Game")
-newGame=QAction("New")
-newGame.setShortcut("F2")
-game.addAction(newGame)
-game.addSeparator()
-beginner=QAction("Beginner")
-beginner.setCheckable(True)
-beginner.setChecked(True)
-game.addAction(beginner)
-intermediate=QAction("Intermediate")
-intermediate.setCheckable(True)
-game.addAction(intermediate)
-expert=QAction("Expert")
-expert.setCheckable(True)
-game.addAction(expert)
-custom=QAction("Custom...")
-custom.setCheckable(True)
-game.addAction(custom)
-game.addSeparator()
-marks=QAction("Marks(?)")
-marks.setCheckable(True)
-marks.setChecked(True)
-game.addAction(marks)
-game.triggered[QAction].connect(buttonPressed)
-window.setGeometry(1280,720,200,50)
-window.setWindowTitle("PyQt")
-window.show()
-pygameThread.setDaemon(True)
-pygameThread.start()
-app.exec_()
-print('ok')
+window=MinesweeperWindow()
+if __name__=="__main__":
+    sys.exit(app.exec_())
